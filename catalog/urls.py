c7f3d5fe3df057fc8402fca9642b5d3f445618a0
bnutss <b.nutss@mail.com>
2026-05-05 15:29:11 +0500

    fixed

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,6 +5,7 @@ app_name = 'catalog'
 
 urlpatterns = [
     path('', views.catalog_list, name='list'),
+    path('search/', views.search_autocomplete, name='search'),
     path('category/<slug:slug>/', views.category_detail, name='category'),
     path('product/<slug:slug>/', views.product_detail, name='product'),
 ]
